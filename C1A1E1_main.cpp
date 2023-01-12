f78de67d7ee207e5451b41ced1bb3720fab58b26
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-11 18:48:14 -0800

    up

--- C1A1E1_main.cpp
+++ C1A1E1_main.cpp
@@ -1,12 +1,16 @@
 // C I C++ Programming I.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+//#include<stdio.h>
+//#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//int main()
+//{
+//    printf("AAA");
+//    std::cout << "Hello World!\n";
+//
+//    return 0;
+//}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
---
+++ C1A1E2_main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//#include <iostream>
+
+int main()
+{
+    printf("A22AA");
+    // std::cout << "Hello World!\n";
+
+    return 0;
+}
