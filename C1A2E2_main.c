fe1b18e1dd8f0aed5fc22c00330f2b4dac2907d0
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-17 19:00:34 -0800

    upd_A2

---
+++ C1A2E2_main.c
@@ -0,0 +1,12 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming I
+// C1 168566 Raymond L. Mitchell
+// 1/17/2023
+// C1A2E2_main.c
+// Win10
+// Visual C++ 2022, ISO C17
+//
+// Assignment #2 C1A2E2 (C)
+//
