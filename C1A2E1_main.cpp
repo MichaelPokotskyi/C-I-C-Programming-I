7333a0774bc0186d368a9b1ed9ae47e92fecdde7
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-17 23:24:41 -0800

    upd

--- C1A2E1_main.cpp
+++ C1A2E1_main.cpp
@@ -9,4 +9,22 @@
 // Visual C++ 2022, ISO C++ 17
 //
 // Assignment #2 C1A2E1 (C++)
-//
+// 
+// If user input anything other then uppercase character, this small programm algorithm will continue work with ASCII - displaying symbol from
+// regular or extended ASCII table keeping given difference, for instance '0'->'P' where:
+//                <P>(0x50) = 0x30(<0> hex) + 0x20(giver diff).
+// Since variable declared as an single char it can hold only one character and will hold first entered value. In case if whitespace will be placed
+// proir any other symbol(s) entered, an '@' will be displayed because <sp>(0x20) + 0x20(giver diff between Upper and Lower) = <@>(0x40).
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char any_symbol{}, upper_symbol{};
+    cout << "Enter any character: ";
+    cin.get(any_symbol);
+    upper_symbol = any_symbol + 0x20;
+    cout << "The lowercase equivalent of " << "'" << any_symbol << "'" << " is " << "'" << upper_symbol << "'" << endl;
+    return 0;
+}
