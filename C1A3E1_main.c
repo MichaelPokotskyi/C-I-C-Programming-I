570a3d2d94dfdfcc2d2a1b7cffe31035c829608d
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-23 21:57:57 -0800

    upd

--- C1A3E1_main.c
+++ C1A3E1_main.c
@@ -10,10 +10,26 @@
 //
 // Assignment #3 C1A3E1 (C)
 //
-//#include <stdio.h>
-//
-//int main(void)
-//{
-//   
-//    return 0;
-//}
+
+#include <stdio.h>
+
+int main(void) {
+
+    int input_pow1, pow7, pow8;
+
+    printf("Enter integer value >= 0: ");
+    scanf_s("%d", &input_pow1);
+
+    printf("n         n^7         n^8 \n-------------------------\n");
+
+    for (int j = 0; j <= input_pow1; j++) {
+        printf("%d", j);
+
+        pow7 = j * j * j * j * j * j * j;
+        printf("%12d", pow7);
+
+        pow8 = j * pow7;
+        printf("%12d\n", pow8);
+    }
+    return 0;
+}
