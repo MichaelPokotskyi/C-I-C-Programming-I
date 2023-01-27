8c117314518fe5dfe7549524c804ddd3995a0fb0
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-27 09:30:59 -0800

    upd

--- C1A3E2_main.cpp
+++ C1A3E2_main.cpp
@@ -11,36 +11,36 @@
 // Assignment #3 C1A3E2 (C++)
 // HEX in reverse mechanic
 // 
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//    const int BASE = 16;
-//    int input_number { };
-//    bool is_neg = false;
-//
-//    cout << "Enter a hexadecimal value: ";
-//    cin >> hex >> input_number;
-//    cout << "\"";
-//    
-//    if (input_number < 0) // negative input?
-//    {
-//        is_neg = true; 
-//        input_number = -input_number;
-//        cout << "-";
-//    }
-//    cout << hex << input_number << "\" in reverse is \""; 
-//    do                    // no special ZERO case implemented
-//    {
-//        int LSDigit = input_number % BASE;
-//        cout << LSDigit;  // output characters in reverse
-//        input_number /= BASE;
-//    } while (input_number > 0);
-//    if (is_neg)           // for minus sign output
-//    { 
-//        cout << "-"; 
-//    }
-//    cout << "\"\n";
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int BASE = 16;
+    int input_number { };
+    bool is_neg = false;
+
+    cout << "Enter a hexadecimal value: ";
+    cin >> hex >> input_number;
+    cout << "\"";
+    
+    if (input_number < 0) // negative input?
+    {
+        is_neg = true; 
+        input_number = -input_number;
+        cout << "-";
+    }
+    cout << hex << input_number << "\" in reverse is \""; 
+    do                    // no special ZERO case implemented
+    {
+        int LSDigit = input_number % BASE;
+        cout << LSDigit;  // output characters in reverse
+        input_number /= BASE;
+    } while (input_number > 0);
+    if (is_neg)           // for minus sign output
+    { 
+        cout << "-"; 
+    }
+    cout << "\"\n";
+    return 0;
+}
