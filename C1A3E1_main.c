abc61a5948a9b425bf449d3793ba443e72288fe3
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-01-27 19:29:17 -0800

    upd

--- C1A3E1_main.c
+++ C1A3E1_main.c
@@ -11,25 +11,28 @@
 // Assignment #3 C1A3E1 (C)
 // PRINTF table formatting for entered number to ^7 and ^8 power
 // with displaying intermediate powers.
+// 
+// For representing numbers bigger then supported by built-in types, 
+// user defined types might be used.
 //
-//
-//#include <stdio.h>
-//
-//int main(void) {
-//    int input_pow1;
-//    printf("Enter integer value >= 0: ");
-//    scanf_s("%d", &input_pow1);
-//    printf(" n^1         n^7         n^8\n"
-//        "------------------------------\n");
-//    for (int pow1 = 0; pow1 <= input_pow1; pow1++) {
-//        int pow7, pow8;
-//        printf("%4d", pow1);
-//        // pow7 done by multiplication as advised
-//        pow7 = pow1 * pow1 * pow1 * pow1 * pow1 * pow1 * pow1;
-//        printf("%12d", pow7);
-//        // pow8 done by multiplication by pow7 to pow1
-//        pow8 = pow1 * pow7;
-//        printf("%12d\n", pow8);
-//    }
-//    return 0;
-//}
+
+#include <stdio.h>
+
+int main(void) {
+    int input_pow1;
+    printf("Enter integer value >= 0: ");
+    scanf_s("%d", &input_pow1);
+    printf(" n^1         n^7         n^8\n"
+        "------------------------------\n");
+    for (int pow1 = 0; pow1 <= input_pow1; pow1++) {
+        int pow7, pow8;
+        printf("%4d", pow1);
+        // pow7 done by multiplication as advised
+        pow7 = pow1 * pow1 * pow1 * pow1 * pow1 * pow1 * pow1;
+        printf("%12d", pow7);
+        // pow8 done by multiplication by pow7 to pow1
+        pow8 = pow1 * pow7;
+        printf("%12d\n", pow8);
+    }
+    return 0;
+}
--- __TEST_UNIT_1.cpp
+++ __TEST_UNIT_1.cpp
@@ -1,21 +1,15 @@
 //#include <iostream>
 //using namespace std;
-//
+
 //int main()
 //{
-//    cout << "Enter a hexadecimal integer value: ";
-//    int input_number { };
-//    cin >> hex >> input_number;
-//    cout << "\"";
-//    bool is_neg = 0;
-//    if (input_number < 0) { is_neg = 1; input_number *= -1; cout << "-"; }
-//    cout << hex << input_number << "\" in reverse is \"";
-//    while (input_number > 0) {
-//        int LSD = input_number % 16;
-//        cout << LSD;
-//        input_number /= 16;
-//    }
-//    if (is_neg) { cout << "-"; }
-//    cout << "\"";
-//    return 0;
+
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+  
 //}
