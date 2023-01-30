3b8bc19db69a59465206de524f2cfed9afd62a3b
Michael Pokotskyi <pokotskyi.m@gmail.com>
2023-01-29 20:13:03 -0800

    o

--- __TEST_UNIT.cpp
+++ __TEST_UNIT.cpp
@@ -1,3 +1,63 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+// #include <stdio.h>
+
+/*
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    const int BASE = 8;
+    int number;
+
+    cout << "Enter octal number: ";
+    cin >> oct >> number;
+    //cout << "\"";
+    //if (number < 0) { cout << "-" ; number *= -1; }
+    //cout << oct << number << "\" in words is \"";
+    //cout << "\n\n\n";
+    
+    int divisor = 1, MSD = 0;
+    for (int dividend = number; dividend > BASE - 1; dividend /= BASE) {
+        //cout << oct << dividend << "\n";
+        divisor *= BASE; // increase divisor by a power of RADIX
+        //dividend /= BASE;
+        // cout << oct << divisor; 10, 100, 1000
+        //LSD = dividend % 8 ;
+        //MSD = number / divisor;
+        //MSD = number / divisor;
+        //dividend /= BASE;
+        } 
+        //cout << MSD << "\n";
+    
+
+    //cout << dividend << "\n";
+
+      MSD = number / divisor;
+      
+    
+    cout << oct <<
+        "Dividing octal " << number << " by octal " << divisor <<
+        " produces its MSD (" << number / divisor << ")\n";
+
+    //while () {
+        int val = number;
+        val /= (MSD);
+        cout << oct << val << "\n";
+        //MSD = MSD * 
+    //}
+
+    //cout << number << " decimal" << " = " << oct << number
+      //  << " octal" << " = " << hex << number << " hexadecimal\n";
+    return 0;
+}
+*/
+=======
+=======
+>>>>>>> 7db969f5ee9426afe233395115549f0fd4c8acaf
 //
 //// #include <stdio.h>
 //
@@ -52,3 +112,7 @@
 //    return 0;
 //
 //}
+<<<<<<< HEAD
+>>>>>>> 7db969f5ee9426afe233395115549f0fd4c8acaf
+=======
+>>>>>>> 7db969f5ee9426afe233395115549f0fd4c8acaf
