2819046810128d76bd8aa296b4a1348be9c5e2cd
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2023-02-01 00:22:41 -0800

    upd4

--- C1A4E4_MaxOf.h
+++ C1A4E4_MaxOf.h
@@ -1,1 +1,13 @@
-#pragma once
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming I
+// C1 168566 Raymond L. Mitchell
+// 1/30/2023
+// C1A4E4_MaxOf.h
+// Win10
+// Visual C++ 2022, ISO C++ 17
+//
+// Assignment #4 C1A4E4 (C++)
+
+
--- C1A4E4_main.cpp
+++ C1A4E4_main.cpp
@@ -4,21 +4,18 @@
 // C I C++ Programming I
 // C1 168566 Raymond L. Mitchell
 // 1/30/2023
-// C1A4E3_main.cpp
+// C1A4E4_main.cpp
 // Win10
 // Visual C++ 2022, ISO C++ 17
 //
-// Assignment #4 C1A4E3 (C++)
-// Function main providing user input for character, character 
-// and lines quantity used as arguments in PrintLines function.
-// Function calls testing default arguments.
+// Assignment #4 C1A4E4 (C++)
 
-//#include <iostream>
-//using namespace std;
+#include <iostream>
+using namespace std;
 
-//int main()
-//{
+int main()
+{
 
-//    }
-//    return 0;
-//}
+    
+    return 0;
+}
