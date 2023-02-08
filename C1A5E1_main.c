d4230e3fb07f13f0f589fc7dce722a1b098b6c76
MiShKa <sly.booking@gmail.com>
2023-02-07 22:45:52 -0800

    upd clr

--- C1A5E1_main.c
+++ C1A5E1_main.c
@@ -9,58 +9,59 @@
 // Visual C++ 2022, ISO C17
 //
 // Assignment #5 C1A5E1 (C)
-// Program 
+// Calculating how many of each rating values was assigned from customer input
+// in a given range. End of customers or given consecutive out of range 
+// attempts used to finish the program.
 
 #include <stdio.h>
 
-#define SHOPPERS 13                                 // number of shoppers
-#define WORST -10                                   // lowest number <= BEST
-#define BEST -4                                     // highest number >= WORST
-#define CHOICES (BEST - WORST + 1)                  // working range
-#define MAX_ERRORS  3                               // out of range attempts
+#define SHOPPERS ('q' - 'd')                       // number of shoppers
+#define WORST (-10)                                // lowest number <= BEST
+#define BEST (-4)                                  // highest number >= WORST
+#define CHOICES (BEST - WORST + 1)                 // working range
+#define MAX_ERRORS  3                              // out of range attempts
 
 int main(void)
 {
-    printf("Allowed rating range: [%d to %d]\nMaximum inputs to terminate: %d\n",
-        BEST, WORST, MAX_ERRORS);
+    printf("Allowed rating range: [%d to %d]\n"
+           "Maximum inputs to terminate: %d\n", 
+           BEST, WORST, MAX_ERRORS);
 
-    int responses[CHOICES] = { 0 };                // storing count of each number
-    int errCount = 0;                              // error counter initialization
-    // Loop to get guest responses.
-    for (int shopper = 1; shopper <= SHOPPERS; ++shopper)
+    int feedbacks[CHOICES] = {0};                  // count for each number
+    int errors = 0;                                // out of range counter
+    for (int shopper = 1; shopper <= SHOPPERS;)
     {
         printf("Shopper %d: ", shopper);
         int userInput;
         scanf_s("%d", &userInput);
         if (shopper == SHOPPERS)
         {
-            printf("\n");                          // extra whitespace before table
+            printf("\n");                          // extra line before table
         }
-
-        // couuect input
+        // correct input
         if (userInput >= WORST && userInput <= BEST)
         {
-            ++responses[userInput - WORST];        // increment response count
-            errCount = 0;                         // refresh error counter
+            ++shopper;
+            ++feedbacks[userInput - WORST];        // increment response count
+            errors = 0;                            // refresh error counter
         }
         // input out of range
         else 
         {
             printf("->Out of range: %d<-\n", userInput);
-            ++errCount;
-            if (errCount == MAX_ERRORS)
+            ++errors;
+            if (errors == MAX_ERRORS)
             {
                 printf("->Survey incomplete: Range error termination<-\n\n");
                 break;
             }
-            shopper -= 1;                         // stay at current index
+            --shopper;                             // stay at current index
             continue;
         }
-
     }
-
     //format table and output numbers
-    printf("Rating       Quantity\n------      --------\n");
+    printf("Rating       Quantity\n"
+           "------       --------\n");
     for (int countArr = CHOICES - 1; countArr >= 0; --countArr)
-        printf("%6d %14d\n", countArr + WORST, responses[countArr]);
+        printf("%6d %14d\n", countArr + WORST, feedbacks[countArr]);
 }
