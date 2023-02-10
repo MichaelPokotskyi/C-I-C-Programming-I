a0e6456876a87357d81e2c5275314fb90bf10006
MiShKa <sly.booking@gmail.com>
2023-02-09 18:30:56 -0800

    uuu

--- C1A5E3_main.cpp
+++ C1A5E3_main.cpp
@@ -14,23 +14,23 @@
 // Each function argument list and returning types are pointers
 // of type double.
 
-#include <iostream>
-using namespace std;
-
-double *ComputeMinimum(const double *nbr1, const double *nbr2);
-double *ComputeMaximum(const double *nbr1, const double *nbr2);
-
-int main()
-{
-    double nbr1, nbr2;
-    cout << "Enter two numbers: ";
-    cin >> nbr1 >> nbr2;
-    // MIN and MAX output statements,
-    // the third value is appropriate function call
-    // returning pointer of type double
-    cout << "ComputeMinimum(&" << nbr1 << ", &" << nbr2 <<
-        ") returned &" << *ComputeMinimum(&nbr1, &nbr2) << "\n";
-    cout << "ComputeMaximum(&" << nbr1 << ", &" << nbr2 <<
-        ") returned &" << *ComputeMaximum(&nbr1, &nbr2) << "\n";
-    return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//double *ComputeMinimum(const double *nbr1, const double *nbr2);
+//double *ComputeMaximum(const double *nbr1, const double *nbr2);
+//
+//int main()
+//{
+//    double nbr1, nbr2;
+//    cout << "Enter two numbers: ";
+//    cin >> nbr1 >> nbr2;
+//    // MIN and MAX output statements,
+//    // the third value is appropriate function call
+//    // returning pointer of type double
+//    cout << "ComputeMinimum(&" << nbr1 << ", &" << nbr2 <<
+//        ") returned &" << *ComputeMinimum(&nbr1, &nbr2) << "\n";
+//    cout << "ComputeMaximum(&" << nbr1 << ", &" << nbr2 <<
+//        ") returned &" << *ComputeMaximum(&nbr1, &nbr2) << "\n";
+//    return 0;
+//}
