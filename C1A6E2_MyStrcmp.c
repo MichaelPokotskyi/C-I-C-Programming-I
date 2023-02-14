148a0492298ef5e0e0be5e2951a43576dd88d972
MiShKa <sly.booking@gmail.com>
2023-02-14 00:32:19 -0800

    upd6

---
+++ C1A6E2_MyStrcmp.c
@@ -0,0 +1,14 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming I
+// C1 168566 Raymond L. Mitchell
+// 2/14/2023
+// C1A6E2_MyStrcmp.c
+// Win10
+// Visual C++ 2022, ISO C17
+//
+// Assignment #6 C1A6E2 (C)
+
+
+
---
+++ C1A6E3_main.c
@@ -0,0 +1,12 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming I
+// C1 168566 Raymond L. Mitchell
+// 2/14/2023
+// C1A6E3_main.c
+// Win10
+// Visual C++ 2022, ISO C17
+//
+// Assignment #6 C1A6E3 (C)
+
