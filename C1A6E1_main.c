66d9e92ec43bfd1b90c873d6b7b917fa3dfd038b
MiShKa <sly.booking@gmail.com>
2023-02-15 17:34:01 -0800

    UPD

--- C1A6E1_main.c
+++ C1A6E1_main.c
@@ -13,20 +13,20 @@
 // function to determine and compare returned value (string length)
 // with custom built function 'MyStrlen' should return same value.
 
-#include <stdio.h>
-#include <string.h> // for strlen()
-
-#define BUF_LENGTH 256
-
-size_t MyStrlen(const char *s1);
-
-int main(void)
-{
-    printf("Enter a string: ");
-    // array of chars
-    char inBuf[BUF_LENGTH];
-    fgets(inBuf, BUF_LENGTH, stdin);
-    inBuf[strcspn(inBuf, "\n")] = '\0';
-    printf("strlen (\"%s\") returned %zu\n", inBuf, strlen(inBuf));
-    printf("MyStrlen (\"%s\") returned %zu\n", inBuf, MyStrlen(inBuf));
-}
+//#include <stdio.h>
+//#include <string.h> // for strlen()
+//
+//#define BUF_LENGTH 256
+//
+//size_t MyStrlen(const char *s1);
+//
+//int main(void)
+//{
+//    printf("Enter a string: ");
+//    // array of chars
+//    char inBuf[BUF_LENGTH];
+//    fgets(inBuf, BUF_LENGTH, stdin);
+//    inBuf[strcspn(inBuf, "\n")] = '\0';
+//    printf("strlen (\"%s\") returned %zu\n", inBuf, strlen(inBuf));
+//    printf("MyStrlen (\"%s\") returned %zu\n", inBuf, MyStrlen(inBuf));
+//}
