0bc69c8fda69ceecc84d59b6de8a527a389de113
MiShKa <sly.booking@gmail.com>
2023-02-20 22:25:33 -0800

    UP7/1

---
+++ C1A7E2_main.c
@@ -0,0 +1,12 @@
+//
+// Michael Pokotskyi U09662201
+// pokotskyi.m@gmail.com
+// C I C++ Programming I
+// C1 168566 Raymond L. Mitchell
+// 2/20/2023
+// C1A7E2_main.c
+// Win10
+// Visual Studio 2022, ISO C17
+//
+// Assignment #7 C1A7E2 (C)
+
--- __TEST_UNIT.cpp
+++ __TEST_UNIT.cpp
@@ -6,16 +6,22 @@
 //}
 
 
-//#include <iostream>
-//using namespace std;
-//
-//
-//int main()
-//{
-//    int* p = (int*)20;
-//    cout << sizeof(p) << "\n";
-//    cout << p << "\n";
-//    ++p;
-//    cout << p << "\n";
-//    cout << sizeof(p);
-//}
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int arr1[5] = {1,2,3,4,5};
+
+    int arr[5][5] = {
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15},
+        {16,17,18,19,20},
+        {21,22,23,24,25}
+    };
+
+    cout << sizeof(arr) / sizeof(arr[0]);
+
+}
